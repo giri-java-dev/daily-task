a91ef45ca2a8b66570f7cd95ca2e9be841b39f4d
Giri Latha <girilatha@gmail.com>
2026-06-11 21:06:31 +0530

    Q2 LastDigit Finding Project was Done

---
+++ src/Q2/LastDigit.java
@@ -0,0 +1,17 @@
+package Q2;
+import java.util.Scanner;
+
+public class LastDigit {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+
+            int lastDigit = num % 10;
+
+            System.out.println("Last Digit is : " + lastDigit);
+
+            sc.close();
+       }
+}
