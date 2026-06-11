b0e8017360eb99f128cf4c3d347d319cdfd3bacb
Giri Latha <girilatha@gmail.com>
2026-06-11 20:56:05 +0530

    Q1 is Done

---
+++ src/Q1/OddOrEven.java
@@ -0,0 +1,18 @@
+package Q1;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Number : ");
+        int num  = sc.nextInt();
+
+        if (num % 2 ==0 ){
+            System.out.println(num + "  is a Even Number");
+        }else {
+            System.out.println(num + "  is a Odd Number");
+        }
+    }
+}
