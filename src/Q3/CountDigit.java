135eeb0bd3c038894d55ddc1e07dbaba49dde22b
Giri Latha <girilatha@gmail.com>
2026-06-12 07:50:57 +0530

    feat: CountDigit Program Added

---
+++ src/Q3/CountDigit.java
@@ -0,0 +1,14 @@
+package Q3;
+
+public class CountDigit {
+    public static void main(String[] args) {
+        int num = 123456;
+        int count=0;
+
+        while (num !=0){
+            num = num / 10;
+            count++;
+        }
+        System.out.println("Number of digits is : "+ count);
+    }
+}
