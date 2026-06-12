8a8ed674521842fbfe6c04fad463df72a9b27175
Giri Latha <girilatha@gmail.com>
2026-06-12 08:09:16 +0530

    feat: Reverse Number Program is Added

---
+++ src/Q4/ReverseNum.java
@@ -0,0 +1,16 @@
+package Q4;
+
+public class ReverseNum {
+    public static void main(String[] args){
+        int num = 123456;
+
+        int reverse = 0;
+
+        while (num !=0){
+            int digit = num % 10;
+            reverse = reverse *10 + digit;
+            num = num /10 ;
+        }
+        System.out.println("Reverse Number is : " + reverse);
+    }
+}
